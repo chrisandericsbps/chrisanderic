47363a2cf364bb5a6f631a98ee33031220eb407c
chrisandericsbps <chrisandericsbps@gmail.com>
2024-10-24 13:46:42 -0400

    Add files via upload

---
+++ recipes/recipePage.js
@@ -0,0 +1,6 @@
+const recipe = document.querySelector('.recipe');
+const el = document.getElementById('select_style');
+el.addEventListener('change', (e) => {
+  recipe.classList.remove('recipe--old-style', 'recipe--modern')
+  recipe.classList.add(`recipe--${e.target.value}`);
+})
